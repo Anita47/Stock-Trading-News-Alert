6fa80d68a9bf48123ba251dd1d3edc801ec3cd32
Anita <117335857+Anita47@users.noreply.github.com>
2023-02-13 15:47:13 +0530

    Update main.py

--- stock_trading_news_alert/main.py
+++ stock_trading_news_alert/main.py
@@ -25,13 +25,18 @@ data_list=[value for (key,value) in data.items()]
 yesterday_data=data_list[0]
 yesterday_closing_price=yesterday_data['4. close']
 
-
+print(yesterday_closing_price)
 
 day_before_yesterday_data=data_list[1]
 day_before_yesterday_closing=day_before_yesterday_data['4. close']
 
+print(day_before_yesterday_closing)
+
 difference=abs(float(yesterday_closing_price)-float(day_before_yesterday_closing))
+print(difference)
+
 diff_percent=(difference/float(yesterday_closing_price))*100
+print(str(diff_percent)+'%')
 
 
 if diff_percent>5:
@@ -53,9 +58,8 @@ if diff_percent>5:
             from_="+12342241648",
             to="Add your phone number"
             )
-print(str(diff_percent)+'%')
 
-print(yesterday_closing_price)
-print(day_before_yesterday_closing)
-print(difference)
+
+
+
 
